dd89bbca716fafca910156b7d1f07535c14c8727
Balaji <81224567+balaji1610@users.noreply.github.com>
2024-04-03 01:42:58 +0530

    Update width,colum visablity

--- src/app/DataGrid/DataGridPage.tsx
+++ src/app/DataGrid/DataGridPage.tsx
@@ -1,8 +1,26 @@
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  useGridApiRef,
+} from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
+import Dropdown from "@/app/common/dropdown";
+
 export default function DataGridPage() {
   const [personalInfo, setPersonalInfo] = useState([]);
-
+  const [loading, setLoading] = useState<boolean | undefined>(true);
+  const InutTopicsOptions = [
+    {
+      label: "Default",
+      value: "Default",
+    },
+    {
+      label: "Communication",
+      value: "Communication",
+    },
+  ];
+  const [inputTopics, setInputTopics] = useState(InutTopicsOptions[0].label);
   const BaseURL = "https://jsonplaceholder.typicode.com/users";
   useEffect(() => {
     (async () => {
@@ -12,6 +30,7 @@ export default function DataGridPage() {
         const response = await getFetch.json();
 
         setPersonalInfo(response);
+        setLoading(false);
       } catch (error) {
         console.error(error);
       }
@@ -26,14 +45,18 @@ export default function DataGridPage() {
       </button>
     );
   };
+
+  const apiRef = useGridApiRef();
+
   const columns: GridColDef[] = [
     {
       field: "id",
       headerName: "ID",
-      width: 150,
+      minWidth: 120,
+      width: 130,
       renderCell: (row) => <CountButton id={row.id} />,
     },
-    { field: "name", headerName: "Name", width: 150 },
+    { field: "name", headerName: "Name", width: 150, resizable: false },
     {
       field: "phone",
       headerName: "Phone",
@@ -43,21 +66,61 @@ export default function DataGridPage() {
 
         return phoneNumberOne[0];
       },
+      valueFormatter: (value: string) => {
+        return value.split("").filter(Number).join("");
+      },
     },
-    { field: "website", headerName: "Website URL", width: 150 },
+    {
+      field: "website",
+      headerName: "Website URL",
+      width: 125,
+      minWidth: 150,
+      maxWidth: 200,
+      hideable: false,
+      renderHeader: () => (
+        <p>
+          {"Website URL"}
+          <span role="img" aria-label="enjoy">
+            🎂
+          </span>
+        </p>
+      ),
+    },
+
     {
       field: "email",
       headerName: "EMAIL",
-      width: 150,
+      width: 125,
+      description:
+        "The identification used by the person with access to the online service",
     },
   ];
 
-  console.log(personalInfo, "personalInfo");
+  const handleDropdownChange = (e: any) => {
+    setInputTopics(e.target.value);
+  };
+
+  const columnVisibilityModel =
+    inputTopics == "Default"
+      ? { id: true, name: true, phone: true, website: true, email: true }
+      : { id: false, name: true, phone: true, website: false, email: true };
+
   return (
     <div>
-      {" "}
+      <Dropdown
+        value={inputTopics}
+        options={InutTopicsOptions}
+        handleDropdownChange={handleDropdownChange}
+      />
       <div style={{ width: "100%" }}>
-        <DataGrid rows={personalInfo} columns={columns} autoHeight />
+        <DataGrid
+          rows={personalInfo}
+          columns={columns}
+          autoHeight
+          loading={loading}
+          apiRef={apiRef}
+          columnVisibilityModel={columnVisibilityModel}
+        />
       </div>
     </div>
   );
